b535ad562f6cd1568de299a77645a8a0918c598e
KeedaByte Programming <68140274+keedabyte@users.noreply.github.com>
2023-12-26 19:36:33 +0000

    added model route

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from starlette.requests import Request
+
 
 # Create an instance of the FastAPI class
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> None:
+    """Asynchronous context manager for managing the lifespan of a FastAPI application."""
+    await startup()
+    yield
+    await shutdown()
+
+app = FastAPI(lifespan=lifespan)
+
+async def startup():
+    print('starting the server')
+    from ultralytics import YOLO
+    app.model = YOLO('yolov8m_detect_usdl.pt')
+
+
+async def shutdown():
+    print('shutting down the server')
 
 # Define a route using a decorator
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 # Define a route with path parameters
-@app.get("/items/{item_id}")
-def read_item(item_id: int, query_param: str = None):
-    return {"item_id": item_id, "query_param": query_param}
+@app.get("/predict")
+async def read_item(request: Request, image_url: str):
+
+    model = request.app.model
+
+    print('model loaded successfully')
+
+    return {'image_url': image_url}
